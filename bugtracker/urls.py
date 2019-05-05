5e0a8d55c6959b34b4ff4ddc68dc65d1ca22ee18
ibtehaz <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-05-05 13:54:54 +0600

    added trailing slash on url

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import url
-from django.urls import path
-from django.contrib import admin
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from . import views
 #
 urlpatterns = [
-    path('login', auth_views.LoginView.as_view(template_name='registration/login.html')),
-    path('logout', auth_views.LogoutView.as_view()),  # Fix it later
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html')),
+    path('logout/', auth_views.LogoutView.as_view()),  # Fix it later
+    # path('accounts/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
 ]
