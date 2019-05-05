ad3bbf22f15264e9ce74d5b49ab678fe6ba534b3
ibtehaz <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-05-05 12:54:31 +0600

    Test login template added, superuser created for local

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -1,7 +1,12 @@
+from django.conf.urls import url
 from django.urls import path
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from . import views
 #
 urlpatterns = [
+    path('login', auth_views.LoginView.as_view(template_name='registration/login.html')),
+    path('logout', auth_views.LogoutView.as_view()),
     path('', views.index, name='index'),
 ]
--- zathura_bugtracker/urls.py
+++ zathura_bugtracker/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('zathura/', include('bugtracker.urls')),
+    path('', include('bugtracker.urls')),
 ]
