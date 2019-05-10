cfc550eb8a71cc55aefcd487a26f7a7ba546d55e
ibtehaz <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-05-10 11:33:57 +0600

    logout api written

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -3,12 +3,13 @@ from django.views.generic import TemplateView
 
 from bugtracker.api.errors import Error
 from bugtracker.api.login import Login
+from bugtracker.api.logout import Logout
 from bugtracker.api.registration import UserRegistration
-from . import views
-#
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('error_log/', Error.as_view(), name='error_log'),
-    path('register/', UserRegistration.as_view(), name='user_registrationx'),
+    path('register/', UserRegistration.as_view(), name='user_registration'),
     path('login/', Login.as_view(), name='user_login'),
+    path('logout/', Logout.as_view(), name='user_logout'),
 ]
