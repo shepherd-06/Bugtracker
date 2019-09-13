ad5ddf9ac6239ac1590d6f93742925950187fa8c
p1r_a_t3 <10481417+p1r-a-t3@users.noreply.github.com>
2019-09-14 00:02:26 +0600

    Removed some urls

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -12,19 +12,18 @@ from bugtracker.api.token_renew import UserTokenRenew
 from bugtracker.api.user_creation_by_admin import UserCreation
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('error_log/', ErrorLog.as_view()),
-    path('error_log/<str:error_pk>/', ErrorLog.as_view()),  # Not allowed to give space after colon (:)
     path('register/', UserRegistration.as_view()),
     path('login/', Login.as_view()),
     path('logout/', Logout.as_view()),
-    path('user_renew/', UserTokenRenew.as_view()),
     path('project/', Project.as_view()),
     path('project/<str:pk>/', Project.as_view()),
+    path('user_creation/', UserCreation.as_view()),
+    path('user_creation/<str:user_email>/', UserCreation.as_view()),
+    path('error_log/', ErrorLog.as_view()),
+    path('error_log/<str:error_pk>/', ErrorLog.as_view()),  # Not allowed to give space after colon (:)
+    path('user_renew/', UserTokenRenew.as_view()),
     path('org/', Org.as_view()),
     path('org/<str:pk>/', Org.as_view()),
     path('project_token_renew/', ProjectTokenCRUD.as_view()),
     path('project_token_renew/<str:project_token>/', ProjectTokenCRUD.as_view()),
-    path('user_creation/', UserCreation.as_view()),
-    path('user_creation/<str:user_email>/', UserCreation.as_view()),
 ]
