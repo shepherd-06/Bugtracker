84402c1ae02002cc6e6d15308bed7154cdcb283a
ibtehaz <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-10-02 15:07:44 +0600

    pre-alpha version added

--- zathura_bugtracker/settings.py
+++ zathura_bugtracker/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&0gi$w@^ju1y^u8ahh3dgyqjs)!%u2u6uw299ij&k5nd4w$zv1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # Never Go Live with Debug - True!
+DEBUG = False  # Never Go Live with Debug - True!
 
-ALLOWED_HOSTS = ['127.0.0.1', 'shepherd06.com', 'bugtracker.shepherd06.com']
+ALLOWED_HOSTS = ['bugtracker.shepherd06.com']
 
 # Application definition
 
