71bebc4290267ebd4e35eded957a58cac0a83185
ibtehaz <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-10-06 01:19:01 +0600

    removed template env  file

--- utility/template_env.py
+++
@@ -1,9 +0,0 @@
-# from django.utils.timezone import template_localtime
-
-# env = Environment(**kwargs)
-# env.filters.update({
-#     'localtime': template_localtime,
-# })
-# env.globals.update({
-#     'localtime': template_localtime,
-# })
