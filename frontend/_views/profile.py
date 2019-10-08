99c35212ab417302cb113b116bc7718676a5478b
ibtehaz <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-10-09 01:06:17 +0600

    profile view complete

--- frontend/_views/profile.py
+++ frontend/_views/profile.py
@@ -1,20 +1,35 @@
+from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
-from token_manager.models import ProjectToken
-from utility.helper import get_team_object, get_user_object, get_common_view_payload
+from team.models import Team
+from utility.helper import get_user_object, get_common_view_payload
 from utility.token_manager import decode_token, protected
 
+
 class ProfileView(View):
-    
+
     @protected
     def get(self, request):
         payload = decode_token(request.COOKIES['access_token'])
         user = get_user_object(username=payload["sub"])
+
+        teams = Team.objects.filter(members__pk=user.pk)
+        team_payload = list()
+        for team in teams:
+            team_payload.append({
+                "team_id": team.team_id,
+                "team_name": team.team_name,
+                "member": True,
+                "admin": True if Team.objects.filter(
+                    team_admins__pk=user.pk) else False
+            })
+            
         
         context = get_common_view_payload(user, user.get_full_name)
         context["user"] = user
-        
+        context["team_payload"] = team_payload
+        context["total_team"] = len(team_payload)
+ 
         return render(request, 'frontend/profile.html', context)
-        
