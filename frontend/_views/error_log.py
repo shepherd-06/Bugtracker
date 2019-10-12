dc5b282e1e6f09ecaf1fde064d5cbf18d82e6274
ibtehaz <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-10-12 23:41:38 +0600

    error log page created

--- frontend/_views/error_log.py
+++ frontend/_views/error_log.py
@@ -2,6 +2,8 @@ from django.views import View
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from error_logger.models import ErrorLog
+
 from utility.token_manager import protected, decode_token
 from utility.helper import get_user_object, get_common_view_payload
 
@@ -12,5 +14,8 @@ class ErrorLogView(View):
     def get(self, request):
         payload = decode_token(request.COOKIES['access_token'])
         user = get_user_object(username=payload["sub"])
+        error_logs = ErrorLog.objects.all()
+        
         context = get_common_view_payload(user, "Error Log")
+        context["error_logs"] = error_logs
         return render(request, 'frontend/error_log.html', context)
