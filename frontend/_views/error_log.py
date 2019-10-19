48f53613f0c3f731bc11488082d2b75dfcc40bea
ibtehaz <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-10-19 21:56:36 +0600

    log - timezone issue fixed

--- frontend/_views/error_log.py
+++ frontend/_views/error_log.py
@@ -7,6 +7,8 @@ from error_logger.models import ErrorLog
 from utility.token_manager import protected, decode_token
 from utility.helper import get_user_object, get_common_view_payload
 
+from datetime import datetime
+
 
 class ErrorLogView(View):
 
@@ -15,22 +17,25 @@ class ErrorLogView(View):
         payload = decode_token(request.COOKIES['access_token'])
         user = get_user_object(username=payload["sub"])
         error_logs = ErrorLog.objects.all()
+        for logs in error_logs:
+            logs.logged_on = datetime.timestamp(logs.logged_on) * 1000
+            logs.updated_on = datetime.timestamp(logs.updated_on) * 1000
 
         context = get_common_view_payload(user, "Error Log")
         context["error_logs"] = error_logs
         context["total_logs"] = ErrorLog.objects.all().count()
         context["current"] = len(error_logs)
         context["titles"] = [
-                                    "#",
-                                    "User",
-                                    "Error Name",
-                                    "Description",
-                                    "Origin",
-                                    "Project",
-                                    "Logged on",
-                                    "Status",
-                                    "Resolved by",
-                                    "Last Updated",
-                                ]
+            "#",
+            "User",
+            "Error Name",
+            "Description",
+            "Origin",
+            "Project",
+            "Logged on",
+            "Status",
+            "Resolved by",
+            "Last Updated",
+        ]
 
         return render(request, 'frontend/log.html', context)
--- frontend/_views/verbose.py
+++ frontend/_views/verbose.py
@@ -6,7 +6,7 @@ from error_logger.models import VerboseLog
 
 from utility.token_manager import protected, decode_token
 from utility.helper import get_user_object, get_common_view_payload
-
+from datetime import datetime
 
 class VerboseView(View):
 
@@ -15,6 +15,9 @@ class VerboseView(View):
         payload = decode_token(request.COOKIES['access_token'])
         user = get_user_object(username=payload["sub"])
         verbose_logs = VerboseLog.objects.all()
+        
+        for logs in verbose_logs:
+            logs.logged_on = datetime.timestamp(logs.logged_on) * 1000
 
         context = get_common_view_payload(user, "Verbose Log")
         context["verbose_logs"] = verbose_logs
